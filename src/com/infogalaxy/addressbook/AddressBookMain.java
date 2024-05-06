7cd3d77393e76a9d9a0e5442c05191a0f5d8b5d4
TanujPatil <tanujpatil9561@gmail.com>
2024-05-06 16:12:10 +0530

    [ADDED]:by Tanuj - Ability to delete a contact based on first name

--- src/com/infogalaxy/addressbook/AddressBookMain.java
+++ src/com/infogalaxy/addressbook/AddressBookMain.java
@@ -32,6 +32,7 @@ public class AddressBookMain {
 	}
 	
 	public void showContact() {
+		if(contact != null) {
 		System.out.println(contact.getFirstName());
 		System.out.println(contact.getLastName());
 		System.out.println(contact.getAddress());
@@ -40,34 +41,50 @@ public class AddressBookMain {
 		System.out.println(contact.getZip());
 		System.out.println(contact.getPhoneNumber());
 		System.out.println(contact.getEmail());
+		} else {
+			System.out.println("The contact is not available");
+		}
 	}
 	
 	public void editContact() {
+		if(contact != null) {
+			System.out.println("Enter First Name: ");
+			String fName = scanner.next();
+			if(fName.equals(contact.getFirstName())) {
+				System.out.println("Enter Last name: ");
+				contact.setLastName(scanner.next());
+				
+				System.out.println("Enter Address");
+				contact.setAddress(scanner.next());
+			
+				System.out.println("Enter city");
+				contact.setCity(scanner.next());
+				
+				System.out.println("Enter State");
+				contact.setState(scanner.next());
+			
+				System.out.println("Enter Zip");
+				contact.setZip(scanner.next());
+				
+				System.out.println("Enter Phone Number");
+				contact.setPhoneNumber(scanner.next());
+			
+				System.out.println("Enter Email");
+				contact.setEmail(scanner.next());
+			} else {
+				System.out.println("Contact is not available");
+			}
+		} else {
+			System.out.println("Contact is not available");
+		}
+	}
+	
+	public void deleteContact() {
 		System.out.println("Enter First Name: ");
 		String fName = scanner.next();
 		if(fName.equals(contact.getFirstName())) {
-			System.out.println("Enter Last name: ");
-			contact.setLastName(scanner.next());
-			
-			System.out.println("Enter Address");
-			contact.setAddress(scanner.next());
-		
-			System.out.println("Enter city");
-			contact.setCity(scanner.next());
-			
-			System.out.println("Enter State");
-			contact.setState(scanner.next());
-		
-			System.out.println("Enter Zip");
-			contact.setZip(scanner.next());
-			
-			System.out.println("Enter Phone Number");
-			contact.setPhoneNumber(scanner.next());
-		
-			System.out.println("Enter Email");
-			contact.setEmail(scanner.next());
-		} else {
-			System.out.println("Contact is not available");
+			contact = null;
+			System.out.println("Contact is successfully deleted! ");
 		}
 	}
 	public static void main(String[] args) {
@@ -78,7 +95,9 @@ public class AddressBookMain {
 		System.out.println("Edit contact");
 		obj.editContact();
 		obj.showContact();
-		
+		System.out.println("Delete contact");
+		obj.deleteContact();
+		obj.showContact();
 	}
 
 }
