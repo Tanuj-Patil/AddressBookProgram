06d8827b0696dee71292f7b8748957defbad160e
TanujPatil <tanujpatil9561@gmail.com>
2024-05-06 15:53:20 +0530

    [ADDED]: by Tanuj - Ability to edit contact based on first name

--- src/com/infogalaxy/addressbook/AddressBookMain.java
+++ src/com/infogalaxy/addressbook/AddressBookMain.java
@@ -41,12 +41,43 @@ public class AddressBookMain {
 		System.out.println(contact.getPhoneNumber());
 		System.out.println(contact.getEmail());
 	}
+	
+	public void editContact() {
+		System.out.println("Enter First Name: ");
+		String fName = scanner.next();
+		if(fName.equals(contact.getFirstName())) {
+			System.out.println("Enter Last name: ");
+			contact.setLastName(scanner.next());
+			
+			System.out.println("Enter Address");
+			contact.setAddress(scanner.next());
+		
+			System.out.println("Enter city");
+			contact.setCity(scanner.next());
+			
+			System.out.println("Enter State");
+			contact.setState(scanner.next());
+		
+			System.out.println("Enter Zip");
+			contact.setZip(scanner.next());
+			
+			System.out.println("Enter Phone Number");
+			contact.setPhoneNumber(scanner.next());
+		
+			System.out.println("Enter Email");
+			contact.setEmail(scanner.next());
+		} else {
+			System.out.println("Contact is not available");
+		}
+	}
 	public static void main(String[] args) {
 		AddressBookMain obj = new AddressBookMain();
-		
+		System.out.println("Add contact");
 		obj.addContact();
 		obj.showContact();
-		
+		System.out.println("Edit contact");
+		obj.editContact();
+		obj.showContact();
 		
 	}
 
